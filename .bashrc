a0a60549843aef1b75f2f50f0e7a9e07c5b99508
Geoff Whatley <me@geoffwhatley.com>
2025-07-25 09:54:55 +1000

    feat: update asdf install to latest

--- .bashrc
+++ .bashrc
@@ -2,11 +2,3 @@
 
 # shellcheck source=.profile
 . "$HOME/.profile"
-
-ASDF_DIR="$MEATBOX_LIBS_DIR/asdf"
-
-# load `asdf`, our version manager
-if [ -d "$ASDF_DIR" ]; then
-  # shellcheck source=libs/asdf/asdf.sh
-  . "$ASDF_DIR/asdf.sh"
-fi
--- scripts/agnostic/install_or_update_asdf.sh
+++ scripts/agnostic/install_or_update_asdf.sh
@@ -3,57 +3,38 @@
 set -eu
 
 install_or_update_asdf() {
-  asdf_dir="$MEATBOX_LIBS_DIR/asdf"
-
   if ! command -v asdf; then
     echo "asdf: installing cli"
 
-    # clone in our version manager
-    git clone https://github.com/asdf-vm/asdf.git "$asdf_dir" >/dev/null
+    if [ "$(uname -s)" = "Darwin" ]; then
+      brew install asdf
+    else
+      git clone https://aur.archlinux.org/asdf-vm.git && cd asdf-vm && makepkg -si
+    fi
 
-    (cd "$asdf_dir" && git checkout "$(git describe --abbrev=0 --tags)") >/dev/null
+    # generate completions
+    mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
 
-    # disable variable checking as `asdf` will blow up when we source it
-    set +u
-
-    # shellcheck source=../../libs/asdf/asdf.sh
-    . "$asdf_dir/asdf.sh"
-
-    set -u
+    asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
   else
     echo "asdf: updating cli"
 
     asdf update >/dev/null || true
   fi
 
-  desired_plugins="dart golang lua nodejs python ruby"
+  desired_plugins="golang lua nodejs python ruby"
 
   # add our version manager plugins
   for plugin in $desired_plugins; do
     if ! asdf list "$plugin" >/dev/null 2>&1; then
       echo "asdf: installing $plugin plugin"
 
-      asdf plugin-add "$plugin" >/dev/null
+      asdf plugin add "$plugin" >/dev/null
     else
       echo "asdf: updating $plugin plugin"
 
-      asdf plugin-update "$plugin" >/dev/null
+      asdf plugin update "$plugin" >/dev/null
     fi
-  done
-
-  if echo "$desired_plugins" | grep -q "nodejs"; then
-    # import the nodejs GPG keys to verify installation
-    bash "$HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring" >/dev/null
-  fi
-
-  # upgrade and use the latest versions of our packages
-  for plugin in $desired_plugins; do
-    latest_version="$(asdf list-all "$plugin" | grep -E '^[0-9.]+$' | tail -n 1)"
-
-    echo "asdf: installing latest version of $plugin@$latest_version"
-
-    asdf install "$plugin" "$latest_version" >/dev/null
-    asdf global "$plugin" "$latest_version" >/dev/null
   done
 }
 
