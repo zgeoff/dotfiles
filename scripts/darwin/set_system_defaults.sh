03532217e43ffcd7e1364bf284e69bebc3ff812a
Geoff Whatley <me@geoffwhatley.com>
2025-07-25 10:07:57 +1000

    fix: remove unused and legacy darwin config

--- scripts/darwin/set_system_defaults.sh
+++ scripts/darwin/set_system_defaults.sh
@@ -23,13 +23,3 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 # set the Finder prefs for showing a few different volumes on the Desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-# hide Safari's bookmark bar
-defaults write com.apple.Safari ShowFavoritesBar -bool false
-
-# set up Safari for dev
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
