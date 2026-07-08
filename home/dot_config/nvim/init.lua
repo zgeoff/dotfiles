a8975a2f2bce3a6f66ede02cc06b5718dc4e51f7
Geoff Whatley <me@geoffwhatley.com>
2026-07-08 13:08:35 +1000

    feat(nvim): enable hybrid relative line numbers (#195)

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -109,11 +109,10 @@ do
   -- NOTE: You can change these options as you wish!
   --  For more options, you can see `:help option-list`
 
-  -- Make line numbers default
+  -- Hybrid line numbers: relative distances for jump counts, with the
+  -- absolute number shown on the cursor line ('number' + 'relativenumber').
   vim.o.number = true
-  -- You can also add relative line numbers, to help with jumping.
-  --  Experiment for yourself to see if you like it!
-  -- vim.o.relativenumber = true
+  vim.o.relativenumber = true
 
   -- Enable mouse mode, can be useful for resizing splits for example!
   vim.o.mouse = 'a'
